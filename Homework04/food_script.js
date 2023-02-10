30a3f02429606a5c26e404ce7e698293a21d73a8
AleksPilskalns <aleksandra.pilskalns@umconnect.umt.edu>
2023-02-10 15:08:38 -0700

    hw

---
+++ Homework04/food_script.js
@@ -0,0 +1,43 @@
+var girl;
+var girlObjects = [];
+var animation = [];
+
+var myArray = [];
+
+
+
+
+function preload() {
+
+    
+  img = loadImage('C:\Users\Aleks\Documents\GitHub\Mart220\Homework04\assets\Dead-(1).png')
+
+    
+
+}
+
+
+
+
+function setup() {
+    createCanvas(1000, 1000);
+  
+  
+   
+    
+    
+}
+
+
+
+
+function draw() {
+    background(20);
+
+    image(img);
+
+
+
+
+
+}
